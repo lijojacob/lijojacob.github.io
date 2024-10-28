b82e91ad0f41e4d1eb614b7845643d3bfeb248ea
Lijo Jacob <lijo_jacob@apple.com>
2024-10-28 10:28:02 -0400

    Header updated

--- about-me/src/components/Header.js
+++ about-me/src/components/Header.js
@@ -2,8 +2,7 @@
 const Header = () => {
     return (
       <header className="text-center py-6">
-        <h1 className="text-4xl font-bold">Lijja's Resume</h1>
-        <p className="text-lg">Email: lijja@example.com | Phone: (123) 456-7890</p>
+        <h1 className="text-4xl font-bold">Lijo Jacob</h1>
       </header>
     );
   };
